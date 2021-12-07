9852eab8a313f5478cda85485585ca2c75363fdf
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-07 22:52:21 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,22 +1,8 @@
-const myCarousel = new Carousel(document.querySelector("#myCarousel"), {
-    Dots: false,
-    on: {
-      init: (carousel) => {
-        // Create references to DOM elements
-        carousel.$index = carousel.$container.querySelector(".carousel_index");
-        carousel.$count = carousel.$container.querySelector(".carousel_count");
-      },
-      refresh: (carousel) => {
-        // Update total count
-        if (carousel.$count) {
-          carousel.$count.innerHTML = carousel.pages.length;
-        }
-      },
-      change: (carousel) => {
-        // Update index of the current page
-        if (carousel.$index) {
-          carousel.$index.innerHTML = carousel.page + 1;
-        }
-      },
-    },
-  });
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  arrow:true,
+});
