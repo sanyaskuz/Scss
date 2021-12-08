4111b51c5c2facbdb49b9056a465f2d34217b706
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-08 20:52:19 +0200

    test

--- js/main.js
+++ js/main.js
@@ -1,8 +1,63 @@
-$('.fade').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  fade: true,
-  cssEase: 'linear',
-  arrow:true,
-});
+// $('.fade').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+//   cssEase: 'linear',
+//   arrow:true,
+// });
+// Slader
+window.onload = function() {
+    new Slider({
+      images: '.gallery .photo img',
+      next: '.gallery .tabs .next',
+      prev: '.gallery .tabs .prev',
+    })
+    function Slider(obj) {
+      this.images = document.querySelectorAll(obj.images)
+      this.btNext = document.querySelector(obj.next)
+      this.btPrev = document.querySelector(obj.prev)
+      // this.interval = obj.interval
+
+      var i = 0
+      
+
+
+      this.prev = function () {
+        console.log('prev', this);
+        this.images[i].className = "";
+        i--
+        if(i<0) {
+          i = this.images.length - 1
+        }
+        this.images[i].className = "shown";
+
+      }
+
+      this.next = function () {
+        console.log('next');
+        this.images[i].className = "";
+        i++
+        if(i>= this.images.length) {
+          i = 0
+        }
+        this.images[i].className = "shown"
+
+
+      }
+
+
+      this.btNext.addEventListener('click', this.next.bind(this))
+      this.btPrev.addEventListener('click', this.prev.bind(this))
+
+
+      // setInterval(this.next.bind(this), this.interval)
+
+
+
+
+
+    }
+}
+    
+   
