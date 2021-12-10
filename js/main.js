59eda9b6c978bf84fd2884c177e0bdaefac9b55c
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-11 01:56:59 +0200

    test

--- js/main.js
+++ js/main.js
@@ -59,5 +59,19 @@ window.onload = function() {
 
     }
 }
+// Бургер-----------------
+const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu){
+    const menuBody = document.querySelector('.main_nav');
+    iconMenu.addEventListener("click", function(e) {
+      document.body.classList.toggle('_lock')
+      iconMenu.classList.toggle('_active');
+      menuBody.classList.toggle('_active');
+
+      
+    });
+
+
+}
     
    
